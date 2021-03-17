570e2e3bb1d424e51aafde45a2ba61f1ed59a279
adarsh2000ec <80823166+adarsh2000ec@users.noreply.github.com>
2021-03-17 17:19:11 +0530

    Add files via upload

---
+++ main.v
@@ -0,0 +1,59 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:56:16 03/17/2021 
+// Design Name: 
+// Module Name:    main 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module combinationalcircuit(userinput,d0,d1,d2,d3,y0,y1,y2,y3,s0,s1,a0,a1,x,a,b,c,I,  y4,y5,y6,y7,y8,y9,y10,y11,sum,carry); 
+input [1:0] userinput; 
+input d0,d1,d2,d3, a0,a1, a,b,c,I; 
+output y0,y1,y2,y3,s0,s1,y8,y9,y10,y11,y4,y5,y6,y7,sum,carry,x; reg y0,y1,y2,y3,s0,s1,x,y4,y5,y6,y7,y8,y9,y10,y11,sum,carry; 
+always@(userinput) 
+begin 
+case(userinput) 
+2'b00: begin //encoder and demux 
+assign s0=d1|d3; 
+assign s1=d2|d3; 
+assign y0=(~s1)&(~s0)&I; 
+assign y1=(~s1)&(s0)&I; 
+assign y2=(s1)&(~s0)&I; 
+assign y3=(s1)&(s0)&I; 
+ end 
+2'b01:begin //decoder and mux 
+ assign y8=(~a0)& (~a1); 
+ assign y9=(~a0)& a1; 
+ assign y10=a0 &( ~a1); 
+assign y11=a0 & a1; 
+ assign x=(y9)|(y8)|(y10)|(y11); 
+ end 
+ 
+2'b10:begin //decoder and full adder 
+ assign y0=(~a)&(~b)&(~c); 
+ assign y1=(~a)&(~b)&(c); 
+ assign y2=(~a)&(b)&(~c); 
+ assign y3=(~a)&(b)&(c); 
+ assign y4=(a)&(~b)&(~c); 
+ assign y5=(a)&(~b)&(c); 
+ assign y6=(a)&(b)&(~c); 
+ assign y7=(a)&(b)&(c); 
+ assign sum=y1|y2|y4|y7; 
+ assign carry=y3|y5|y6|y7; 
+ end 
+endcase 
+end 
+endmodule 
+
---
+++ testbench.v
@@ -0,0 +1,85 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   16:57:17 03/17/2021
+// Design Name:   combinationalcircuit
+// Module Name:   testbench.v
+// Project Name:  external
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: combinationalcircuit
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module combcircuit_v; 
+// Inputs 
+reg [1:0] userinput; 
+reg d0, d1, d2, d3; 
+reg a0,a1, a, b,c,I; 
+// Outputs 
+wire y0, y1,y2,y3, s0,s1, x, y4, y5, y6, y7,y8,y9,y10,y11, sum, carry; 
+// Instantiate the Unit Under Test (UUT) 
+combinationalcircuit uut (.userinput(userinput), .d0(d0), .d1(d1), .d2(d2), .d3(d3), .y0(y0), .y1(y1), .y2(y2), .y3(y3), .s0(s0), .s1(s1), .a0(a0), .a1(a1), .x(x), .a(a),  .b(b), .c(c), .y4(y4), .y5(y5), .y6(y6), .y7(y7), .y8(y8), .y9(y9), .y10(y10), .y11(y11),  .sum(sum), .I(I),.carry(carry)); 
+initial begin 
+userinput=0; 
+$display("user input=%b",userinput); 
+$display("encoder and demux"); 
+// Initialize Inputs 
+d0 = 1;d1 = 0;d2 = 0;d3= 0;I=1;#50; 
+$display("d0=%d|d1=%d|d2=%d|d3=%d|I=%d|encoder output--- S1=%d|s0=%d| demux  output--- y3=%d|y2=%d|y1=%d|y0=%d|",d0,d1,d2,d3,I,s1,s0,y3,y2,y1,y0,$time); d0 = 0;d1 = 1;d2 = 0;d3 = 0;I=1;#50;
+$display("d0=%d|d1=%d|d2=%d|d3=%d|I=%d|encoder output--- S1=%d|s0=%d| demux  output--- y3=%d|y2=%d|y1=%d|y0=%d|",d0,d1,d2,d3,I,s1,s0,y3,y2,y1,y0,$time); d0 = 0;d1 = 0;d2 = 1;d3 = 0;I=1;#50; 
+$display("d0=%d|d1=%d|d2=%d|d3=%d|I=%d|encoder output--- S1=%d|s0=%d| demux  output--- y3=%d|y2=%d|y1=%d|y0=%d|",d0,d1,d2,d3,I,s1,s0,y3,y2,y1,y0,$time); d0 = 0;d1 = 0;d2 = 0;d3 = 1;I=1;#50; 
+$display("d0=%d|d1=%d|d2=%d|d3=%d|I=%d|encoder output--- S1=%d|s0=%d| demux  output--- y3=%d|y2=%d|y1=%d|y0=%d|",d0,d1,d2,d3,I,s1,s0,y3,y2,y1,y0,$time);   
+userinput=1; 
+$display("user input=%b",userinput); 
+$display("decoder and mux"); 
+// Initialize Inputs 
+a0 = 0;a1 = 0;#10; 
+$display("a0=%d|a1=%d|decoder op ---y8=%d|y9=%d|y10=%d|y11=%d|mux--- x=%d|",a0,a1,y8,y9,y10,y11,x,$time); 
+a0 = 0;a1 = 1;#10; 
+$display("a0=%d|a1=%d|decoder op ---y8=%d|y9=%d|y10=%d|y11=%d|mux--- x=%d|",a0,a1,y8,y9,y10,y11,x,$time); 
+a0 = 1;a1 = 0;#10; 
+$display("a0=%d|a1=%d|decoder op ---y8=%d|y9=%d|y10=%d|y11=%d|mux--- x=%d|",a0,a1,y8,y9,y10,y11,x,$time); 
+a0 = 1;a1 = 1;#10; 
+$display("a0=%d|a1=%d|decoder op ---y8=%d|y9=%d|y10=%d|y11=%d|mux--- x=%d|",a0,a1,y8,y9,y10,y11,x,$time); 
+  
+userinput=2; 
+$display("user input=%b",userinput); 
+$display(" decoder and fulladder"); 
+a = 0;b = 0;c = 0;#50; 
+$display("|a=%d||b=%d|c=%d|decoder op---- 
+|y0=%d||y1=%d||y2=%d|y3=%d||y4=%d|y5=%d|y6=%d||y7=%d|full adder---- |sum=%d|carry=%d|",a,b,c,y0,y1,y2,y3,y4,y5,y6,y7,sum,carry,$time); 
+a = 0;b = 0;c = 1;#50; 
+$display("|a=%d||b=%d|c=%d|decoder op---- 
+|y0=%d||y1=%d||y2=%d|y3=%d||y4=%d|y5=%d|y6=%d||y7=%d|full adder---- |sum=%d|carry=%d|",a,b,c,y0,y1,y2,y3,y4,y5,y6,y7,sum,carry,$time); 
+a = 0;b = 1;c = 0;#50; 
+$display("|a=%d||b=%d|c=%d|decoder op---- 
+|y0=%d||y1=%d||y2=%d|y3=%d||y4=%d|y5=%d|y6=%d||y7=%d|full adder---- |sum=%d|carry=%d|",a,b,c,y0,y1,y2,y3,y4,y5,y6,y7,sum,carry,$time);
+a = 0;b = 1;c = 1;#50; 
+$display("|a=%d||b=%d|c=%d|decoder op---- 
+|y0=%d||y1=%d||y2=%d|y3=%d||y4=%d|y5=%d|y6=%d||y7=%d|full adder---- |sum=%d|carry=%d|",a,b,c,y0,y1,y2,y3,y4,y5,y6,y7,sum,carry,$time); 
+a = 1;b = 0;c = 0;#50; 
+$display("|a=%d||b=%d|c=%d|decoder op---- 
+|y0=%d||y1=%d||y2=%d|y3=%d||y4=%d|y5=%d|y6=%d||y7=%d|full adder---- |sum=%d|carry=%d|",a,b,c,y0,y1,y2,y3,y4,y5,y6,y7,sum,carry,$time); 
+a = 1;b = 0;c = 1;#50; 
+$display("|a=%d||b=%d|c=%d|decoder op---- 
+|y0=%d||y1=%d||y2=%d|y3=%d||y4=%d|y5=%d|y6=%d||y7=%d|full adder---- |sum=%d|carry=%d|",a,b,c,y0,y1,y2,y3,y4,y5,y6,y7,sum,carry,$time); 
+a = 1;b = 1;c = 0;#50; 
+$display("|a=%d||b=%d|c=%d|decoder op---- 
+|y0=%d||y1=%d||y2=%d|y3=%d||y4=%d|y5=%d|y6=%d||y7=%d|full adder---- |sum=%d|carry=%d|",a,b,c,y0,y1,y2,y3,y4,y5,y6,y7,sum,carry,$time); 
+a = 1;b = 1;c = 1;#50; 
+$display("|a=%d||b=%d|c=%d|decoder op---- 
+|y0=%d||y1=%d||y2=%d|y3=%d||y4=%d|y5=%d|y6=%d||y7=%d|full adder---- |sum=%d|carry=%d|",a,b,c,y0,y1,y2,y3,y4,y5,y6,y7,sum,carry,$time); 
+end 
+endmodule
